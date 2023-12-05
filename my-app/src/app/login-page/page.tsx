c86e861e1f6db24832b0c84a7779456dc2132a35
MatheusPese <matheuspese@outlook.com>
2023-12-04 23:05:56 -0300

    contêiner de formulário centralizado

--- my-app/src/app/login-page/page.tsx
+++ my-app/src/app/login-page/page.tsx
@@ -7,8 +7,8 @@ import { useRouter } from 'next/navigation';
 const buttonStyles = 'block p-2 bg-black bg-opacity-20 text-white px-14 py-2 mx-auto';
 const inputStyles = 'block w-full rounded-md border-0 py-1.5 mb-5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
 const backgroundStyles = 'bg-gradient-to-bl from-blue-400 via-teal-600 to-teal-300';
-const formContainerStyles = 'flex-row pt-10 pb-11 px-7 bg-black bg-opacity-20 rounded-lg z-0';
-const connectTextStyles = 'text-white z-10 mb-5';
+const formContainerStyles = 'm-auto pt-10 pb-11 px-7 bg-black bg-opacity-20 rounded-lg';
+const connectTextStyles = 'text-white mb-5';
 
 const LoginButton: React.FC = () => {
   return (
