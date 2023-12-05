363675a2f8354012b8bc87776f2b03bc7b5e2a25
MatheusPese <matheuspese@outlook.com>
2023-12-04 23:44:35 -0300

    changed screen size to resemble phone

--- my-app/src/app/components/button.tsx
+++ my-app/src/app/components/button.tsx
@@ -11,7 +11,8 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
   return (
     <button
-      className={`max-w-md bg-gray-400 hover:bg-gray-500 bg-opacity-70 text-white font-bold py-5 w-full rounded ${className}`}
+      style={{ maxWidth: '14rem' }}
+      className={`bg-gray-400 hover:bg-gray-500 bg-opacity-70 text-white font-bold py-5 w-full rounded ${className}`}
       onClick={onClick}
     >
       {children}
