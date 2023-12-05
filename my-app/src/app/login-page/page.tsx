047fcbbff503b3d4f47ff38b8b3874956ed81ce1
MatheusPese <matheuspese@outlook.com>
2023-12-04 22:23:23 -0300

    removed login-form component, and modified login-page

--- my-app/src/app/login-page/page.tsx
+++ my-app/src/app/login-page/page.tsx
@@ -1,13 +1,73 @@
-import LoginForm from '../components/login-form'
+'use client'
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+
+// Extracted styles as constants
+const buttonStyles = 'block p-2 bg-black bg-opacity-20 text-white px-14 py-2 mx-auto';
+const inputStyles = 'block w-full rounded-md border-0 py-1.5 mb-5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6';
+const backgroundStyles = 'bg-gradient-to-bl from-blue-400 via-teal-600 to-teal-300';
+const formContainerStyles = 'flex-row pt-10 pb-11 px-7 bg-black bg-opacity-20 rounded-lg z-0';
+const connectTextStyles = 'text-white z-10 mb-5';
+
+const LoginButton: React.FC = () => {
+  return (
+    <button
+      id='entrar'
+      type='submit'
+      className={buttonStyles}
+    >
+      Entrar
+    </button>
+  );
+};
+
+const IdInput: React.FC = () => {
+  return (
+    <input
+      id='phone-or-mail'
+      name='phone-or-mail'
+      type='text'
+      autoComplete='phone-or-mail'
+      required
+      placeholder='E-mail ou Telefone'
+      className={inputStyles}
+    />
+  );
+};
+
+const PasswordInput: React.FC = () => {
+  return (
+    <input
+      id='password'
+      name='password'
+      type='password'
+      autoComplete='password'
+      required
+      placeholder='Senha'
+      className={inputStyles}
+    />
+  );
+};
 
 export default function LoginPage() {
-    
-  const BackgroundClasses ="bg-gradient-to-bl from-blue-400 via-teal-600 to-teal-300"
-  return (  
-    <main className= {`${BackgroundClasses} flex min-h-screen flex-col items-center justify-between`}>
-      <LoginForm/>
-    </main>
-    
-  )
-}
+  const router = useRouter();
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push('/business-panel');
+  };
+
+  return (
+    <main className={`${backgroundStyles} flex min-h-screen flex-col items-center justify-between`}>
+      <div className={formContainerStyles}>
+        <div className={connectTextStyles}>Conectar</div>
+        <form onSubmit={handleSubmit}>
+          <IdInput />
+          <PasswordInput />
+          <LoginButton />
+        </form>
+      </div>
+    </main>
+  );
+}
