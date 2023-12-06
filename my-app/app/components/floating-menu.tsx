bf2141aab4665e9a337ca52906cce7e3b09924ad
MatheusPese <matheuspese@outlook.com>
2023-12-06 01:07:04 -0300

    added floating menu

--- my-app/app/components/floating-menu.tsx
+++ my-app/app/components/floating-menu.tsx
@@ -1,15 +1,33 @@
+'use client'
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface FloatingMenuProps {
     
 }
- 
+
+
+
+
 const FloatingMenu: React.FC<FloatingMenuProps> = () => {
+    const router = useRouter();
+    const Menu1 = () =>{
+        router.push('/pages/business-panel');
+    };
+    const Menu2 = () =>{
+        router.push('/pages/profile');
+    };
+    const Menu3 = () =>{
+        router.push('/pages/login-page');
+    };
+
+
+
     return ( 
-    <div className="bg-white w-20 h-20 m-auto">
-        <button>Tela Inicial</button>
-        <button>Perfil</button>
-        <button>Desconectar-se</button>
+    <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-60 rounded-lg m-auto p-0 text-black flex-row items-center ">
+        <button onClick={Menu1} className="flex p-10 text-xl font-bold">Tela Inicial</button>
+        <button onClick={Menu2} className="flex p-10 text-xl font-bold">Perfil</button>
+        <button onClick={Menu3} className="flex p-10 text-xl text-red-600 font-bold">Desconectar-se</button>
     </div> 
     );
 }
--- my-app/app/pages/business-panel/page.tsx
+++ my-app/app/pages/business-panel/page.tsx
@@ -1,35 +1,41 @@
 'use client'
-
-import PageTemplate from '@/app/components/page-template'
-import Button from '@/app/components/button'
+import { useState } from 'react';
+import PageTemplate from '@/app/components/page-template';
+import Button from '@/app/components/button';
+import FloatingMenu from '@/app/components/floating-menu';
 
 export default function Home() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const MenuClick = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  const Top = (
+    <div className='mx-auto text-2xl'>Negócios</div>
+  );
+
+  const Middle = (
+    menuVisible && (
+      <FloatingMenu />
+    )
+  );
+
+  const Bottom = (
+    <div className='mx-auto flex w-full justify-between'>
+      <Button className='flex-2 m-2' onClick={MenuClick}>
+        Menu
+      </Button>
+    </div>
+  );
+
   return (
     <PageTemplate>
       {{
         Top: Top,
         Middle: Middle,
-        Bottom: Bottom
+        Bottom: Bottom,
       }}
     </PageTemplate>
-
-  )
+  );
 }
-
-const MenuClick = () => {
-  console.log('Button clicked!'); // Replace this with your desired logic
-};
-
-const Top = (
-  <div className='mx-auto text-2xl'>Negócios</div>
-);
-
-const Middle = (
-  <div></div>
-);
-
-const Bottom = (
-  <div className='mx-auto flex w-full justify-between'>
-    <Button className='flex-2 m-2' onClick={MenuClick}>Menu</Button>
-  </div>
-);
