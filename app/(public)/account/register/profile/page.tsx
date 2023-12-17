180d60d68171944ef6ebc3b70f6a518b3e346826
matheuspese <matheuspese@outlook.com>
2023-12-16 21:56:47 -0300

    update register page (WIP)

--- app/(public)/account/register/profile/page.tsx
+++ app/(public)/account/register/profile/page.tsx
@@ -1,20 +1,37 @@
 "use client";
 
 import type { NextPage } from "next";
+import { useForm } from "react-hook-form";
+import { useUserService } from '@/app/_services';
 
-const RegisterPage: NextPage = () => {
+const Register: NextPage = () => {
+  const userService = useUserService();
+  const { register, handleSubmit, formState } = useForm();
+  const { errors } = formState;
+
+  const fields = {
+    firstName: register("firstName", { required: "First Name is required" }),
+    lastName: register("lastName", { required: "Last Name is required" }),
+    email: register("email", { required: "E-mail is required" }),
+    phone: register("phone", { required: "Phone is required" }),
+  };
+  
+  async function onSubmit(user: any) {
+    await userService.register(user);
+  };
+
   return (
     <>
       <h3>Cadastro</h3>
-      <form className="flex flex-col gap-3">
-        <input type="text" placeholder="Nome" />
-        <input type="text" placeholder="Sobrenome" />
-        <input type="email" placeholder="E-mail" />
-        <input type="tel" placeholder="Telefone" />
-        <button>Confirmar</button>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+        <input {...fields.firstName} type="text" placeholder="Nome" />
+        <input {...fields.lastName} type="text" placeholder="Sobrenome" />
+        <input {...fields.email} type="email" placeholder="E-mail" />
+        <input {...fields.phone} type="tel" placeholder="Telefone" />
+        <button disabled={formState.isSubmitting}>Confirmar</button>
       </form>
     </>
   );
 };
 
-export default RegisterPage;
+export default Register;
