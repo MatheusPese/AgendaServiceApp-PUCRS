6ce194cfe12b6b0b2d40b8ac427ab0cd0a8efcbb
MatheusPese <matheuspese@outlook.com>
2023-12-22 09:51:08 -0300

    Create: Appointments page and components

--- app/(secure)/appointments/page.tsx
+++ app/(secure)/appointments/page.tsx
@@ -1,85 +1,111 @@
 "use client";
 
-import Button from "@/app/_components/Button";
-import PageTemplate from "@/app/_components/PageTemplate";
-import AppointmentCard from "@/app/_components/business/AppointmentCard";
-import FloatingMenu from "@/app/_components/FloatingMenu";
-import { useState } from "react";
+import { useState }       from "react";
+
+import Button             from "@/app/_components/Button";
+import FloatingMenu       from "@/app/_components/FloatingMenu";
+import PageTemplate       from "@/app/_components/PageTemplate";
+import AppointmentCard    from "@/app/_components/business/AppointmentCard";
 import NewAppointmentCard from "@/app/_components/business/NewAppointmentCard";
 
 interface Appointment {
-  _id: string;
-  time: string;
-  employee: string;
-  client: string;
-  services: string[];
+  _id      : string;
+  time     : string;
+  employee : string;
+  client   : string;
+  services : string[];
 }
 
 export default function Home() {
-  let appointments = [
-    {
-      _id: 1,
-      time: new Date("2014-03-01T01:00:00"),
-      employee: "Etiane",
-      client: "Maria",
-      services: ["Manicure", "Pedicure"],
-    },
-    {
-      _id: 2,
-      time: new Date(),
-      employee: "Carmen",
-      client: "Joana Alcatra Acelva de Assis Vieira",
-      services: ["Depilação", "Pedicure"],
-    },
-    {
-    _id: 3,
-    time: new Date(),
-    employee: "Carmen",
-    client: "Joana Alcatra Acelva de Assis Vieira",
-    services: ["Depilação", "Pedicure"],
-  },
-];
-  appointments.sort((a, b) => b.time.getTime() - a.time.getTime());
-
-
+  
   const [menuVisible, setMenuVisible] = useState(false);
   
-  const MenuClick = () => {
+  const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
+  
+  let appointments = [
+    {
+      _id: '1',
+      employee: 'Alice Silva',
+      client: 'João Oliveira',
+      time: new Date('2023-12-22T10:00:00'),
+      services: ['Depilação', 'Manicure'],
+    },
+    {
+      _id: '2',
+      employee: 'Carlos Santos',
+      client: 'Joana Alcatra Acelva de Assis Vieira',
+      time: new Date('2023-12-22T11:30:00'),
+      services: ['Coloração de Cabelo', 'Corte de Cabelo', 'Escova Progressiva'],
+    },
+    {
+      _id: '3',
+      employee: 'Fernanda Lima',
+      client: 'Pedro Rocha',
+      time: new Date('2023-12-22T13:15:00'),
+      services: ['Maquiagem', 'Pedicure'],
+    },
+    {
+      _id: '4',
+      employee: 'Gabriel Oliveira',
+      client: 'Juliana Costa',
+      time: new Date('2023-12-22T15:00:00'),
+      services: ['Corte Masculino', 'Barba'],
+    },
+    {
+      _id: '5',
+      employee: 'Larissa Silva',
+      client: 'Lucas Santos',
+      time: new Date('2023-12-22T16:45:00'),
+      services: ['Escova Progressiva', 'Manicure'],
+    },
+  ];
+  
+  
+  appointments.sort((a, b) => b.time.getTime() - a.time.getTime());
+  
+  
+  
 
-
+  
   const Header = <h2 className="text-2xl">Negócios</h2>;
-
+  
   const Body = (
-    <div className="grid gap-3 align-content-around justify-content-between content-around justify-around grid-cols-3 p-3"> 
-    <NewAppointmentCard/>
-    {appointments.map((index, time ) => (
-            <AppointmentCard time={index.time} employee={index.employee} client={index.client} services={index.services}/>
-        ))}
-              {menuVisible && <FloatingMenu />}
+    <div className="grid gap-3 align-content-around justify-content-between content-around justify-around grid-cols-3 p-3">
+      
+      <NewAppointmentCard />
+      
+      {appointments.map((index, time) => (
+        <AppointmentCard
+          time     = {index.time}
+          employee = {index.employee}
+          client   = {index.client}
+          services = {index.services}
+        />
+      ))}
+      
+      {menuVisible && <FloatingMenu />}
+    
     </div>
   );
-
+  
   const Footer = (
-    <div className=" flex w-full justify-between gap-3">
-      <Button onClick={MenuClick}>
-        Menu
-      </Button>
-      <Button >Compartilhar</Button>
+    <div className="flex w-full justify-between gap-3">
+      <Button onClick={toggleMenu}>Menu</Button>
+      <Button>Compartilhar</Button>
     </div>
   );
-
+  
   return (
     <>
-
-<PageTemplate classesBody="bg-[rgba(34, 34, 34, 0.3)] w-full items-start">
-      {{
-        Header,
-        Body,
-        Footer,
-      }}
-    </PageTemplate>
+      <PageTemplate classesBody="bg-[rgba(34, 34, 34, 0.3)] w-full items-start">
+        {{
+          Header,
+          Body,
+          Footer,
+        }}
+      </PageTemplate>
     </>
   );
 }
---
+++ app/_components/business/NewAppointmentCard.tsx
@@ -0,0 +1,21 @@
+// components/Button.tsx
+import {format, isToday, isTomorrow} from 'date-fns';
+import React, { ReactNode } from "react";
+
+
+
+const NewAppointmentCard: React.FC = () => {
+
+    return (
+
+    <button className="btn bg-[rgb(62,152,255)] hover:bg-[rgb(112,172,230)] flex flex-col justify-center items-center w-[6rem] h-[6rem] rounded-3xl shadow-xl p-2 overflow-hidden">
+
+
+        <span className={`text-4xl font-extrabold  text-white`}>+</span>
+
+    
+    </button>
+  );
+};
+
+export default NewAppointmentCard;
