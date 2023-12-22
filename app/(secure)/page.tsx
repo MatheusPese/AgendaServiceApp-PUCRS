d7d46c90af94bc243cb64309f64af28634d0ac85
MatheusPese <matheuspese@outlook.com>
2023-12-22 07:05:59 -0300

    fix: Change page route of autenticated user

--- app/(secure)/page.tsx
+++ app/(secure)/page.tsx
@@ -1,29 +1,24 @@
 'use client';
 
-import Link from 'next/link';
+
 import { useEffect } from 'react';
 
 import { useUserService } from '@/app/_services';
+import { redirect } from 'next/navigation';
+
 
 export default Home;
 
 function Home() {
     const userService = useUserService();
     const user = userService.currentUser;
-
     useEffect(() => {
         userService.getCurrent();
     }, []);
 
     if (user) {
-        return (
-            <>
-                <h1>Hi {user.firstName}!</h1>
-                <p>You&apos;re logged in with Next.js & JWT!!</p>
-                <p><Link href="/users">Manage Users</Link></p>
-            </>
-        );
+        redirect('/business-panel')
     } else {
-        return <p>loading</p>;
+        return <p>loading... <span className="spinner-border spinner-border-sm me-1"></span></p>;
     }
 }
