1782c4a48618e9e1454588892b7aa10f5738b431
MatheusPese <matheuspese@outlook.com>
2023-12-22 02:12:34 -0300

    Documentation: Add comments and explanations for better code understanding in Register

--- app/(public)/account/register/page.tsx
+++ app/(public)/account/register/page.tsx
@@ -1,24 +1,28 @@
+// Directive used to delare a boundary between a server and a client component module.
 "use client";
 
+// Import necessary modules and components
 import type { NextPage } from "next";
 import { useForm } from "react-hook-form";
 import { useUserService } from "@/app/_services";
 import { useState } from "react";
 import RegisterFormInputs from "@/app/_components/account/register/RegisterFormInputs";
 
+// Define the Register functional component
 const Register: NextPage = () => {
-  //HOOKS
+  // HOOKS
 
-  //useUserService hooks
+  // useUserService hooks
   const userService = useUserService();
 
-  //--useForm hooks
+  // useForm hooks
   const { register, handleSubmit, formState } = useForm();
   const { errors } = formState;
 
-  //--useState hooks
+  // useState hooks
   const [isProfile, setIsProfile] = useState(true);
 
+  // Define form fields with validation rules
   const fields = {
     firstName: register("firstName", { required: "First Name is required" }),
     lastName: register("lastName", { required: "Last Name is required" }),
@@ -36,21 +40,26 @@ const Register: NextPage = () => {
     }),
   };
 
+  // Function to toggle between profile and password pages
   const changePage = () => {
     setIsProfile(!isProfile);
-    
   };
 
+  // Function to handle form submission
   const onSubmit = async (user: any) => {
     await userService.register(user);
     console.log(user);
   };
+
+  // Render the component
   return (
     <>
       <div className="flex-1 flex flex-col justify-center items-center">
         <form className="box-container" onSubmit={handleSubmit(onSubmit)}>
+          {/* Render profile form if isProfile is true */}
           {isProfile && (
             <>
+              {/* Profile form */}
               <h3>Cadastro</h3>
               <RegisterFormInputs
                 firstNameProp={fields.firstName}
@@ -58,6 +67,7 @@ const Register: NextPage = () => {
                 emailProp={fields.email}
                 phoneProp={fields.phone}
               />
+              {/* Navigation buttons */}
               <div className="flex flex-row justify-between gap-2 pt-4">
                 <Link href="/account/login" className="btn btn-outline-primary">
                   Cancelar
@@ -68,13 +78,16 @@ const Register: NextPage = () => {
               </div>
             </>
           )}
+          {/* Render password form if isProfile is false */}
           {!isProfile && (
             <>
+              {/* Password form */}
               <h3>Cadastro - Senha</h3>
               <RegisterFormInputs
                 passwordProp={fields.password}
                 repeatedPasswordProp={fields.password_confirmation}
               />
+              {/* Navigation buttons */}
               <div className="flex flex-row justify-end">
                 <button className="btn btn-primary" onClick={changePage}>
                   Voltar
@@ -84,6 +97,7 @@ const Register: NextPage = () => {
                   disabled={formState.isSubmitting}
                   className="btn btn-success"
                 >
+                  {/* Show spinner while form is submitting */}
                   {formState.isSubmitting && (
                     <span className="spinner-border spinner-border-sm me-1"></span>
                   )}
@@ -98,6 +112,5 @@ const Register: NextPage = () => {
   );
 };
 
+// Export the Register component as the default export
 export default Register;
-
-//TODO: Arrumar uma forma de voltar facilmente para a tela de login, sem precisar cadastrar caso o usuário tenha clicado acidentalmente.
