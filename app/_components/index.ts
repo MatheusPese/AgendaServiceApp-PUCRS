6620b5e29f241d04ca1c94b58a929a285b3dc805
MatheusPese <matheuspese@outlook.com>
2023-12-26 23:40:31 -0300

    changed exports from components

--- app/_components/index.ts
+++ app/_components/index.ts
@@ -1,6 +1,13 @@
+// Export Globals
 export * from './globals/Button'
 export * from './globals/FloatingMenu'
 export * from './globals/PageTemplate'
 
-
+// Export from account route
+export * from './account/register/RegisterForms'
 export * from './account/profile/CredentialCard'
+
+// Export from agendas route
+export * from './agendas/AgendaCard'
+export * from './agendas/agenda/AppointmentCard'
+
