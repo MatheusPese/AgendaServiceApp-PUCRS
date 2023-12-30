9d8257eb1e067fad6effd0e9ac31a7555a784438
MatheusPese <matheuspese@outlook.com>
2023-12-30 05:54:48 -0300

    fixed component to handle form submits

--- app/_components/globals/Popup.tsx
+++ app/_components/globals/Popup.tsx
@@ -7,20 +7,23 @@ import Button from "./Button";
 type props = {
 
 	title: string,
-	children?: React.ReactNode
+	children?: React.ReactNode,
+	onConfirm?: (params?:any) => void,
+	onDeny?: (params?:any) => void,
+	onSubmit?: (params?:any) => void
 }
 
-const FloatingMenu: React.FC<props> = ({title = "Title", children}) => {
+const Popup: React.FC<props> = ({title = "Title", children, onConfirm, onDeny, onSubmit}) => {
 	return ( 
 		<div className="flex fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-60 rounded-lg m-auto text-black flex-col items-center gap-4 p-3">
 			<h2 className="text-2xl">{title}</h2>
-			<form className="flex flex-col gap-2">
-				<div>{children}</div>
+			<form className="flex flex-col gap-2" onSubmit={onSubmit}>
+				{children}
 				<div className="flex flex-row gap-2 m-2 ">
-				<Button type="button" customStyle="secondary" size="small" className="flex btn btn-primary">
+				<Button type="button" customStyle="secondary" size="small" className="flex btn btn-primary" onClick={onDeny}>
 					Cancelar
 				</Button>
-				<Button type="button" customStyle="primary" size="small" className="flex btn btn-primary">
+				<Button type="submit" customStyle="primary" size="small" className="flex btn btn-primary" onClick={onConfirm}>
 					Criar
 				</Button>
 				</div>
@@ -30,4 +33,4 @@ const FloatingMenu: React.FC<props> = ({title = "Title", children}) => {
 	);
 }
 
-export default FloatingMenu;
+export default Popup;
