e39070b5c1f56e178f0a3717b5f9d74c8c10dd83
MatheusPese <matheuspese@outlook.com>
2023-12-31 08:34:26 -0300

    Added redirection if the user are unidentified
    (I really don't know why sometimes it is unidentified, but i also don't have time to debug)

--- app/(secure)/agendas/page.tsx
+++ app/(secure)/agendas/page.tsx
@@ -1,33 +1,48 @@
 "use client";
 
+// #region Imports
 import { useEffect, useState } from "react";
+import { redirect } from 'next/navigation';
+import { useAgendaService, useUserService } from "@/app/_services";
+import { useForm } from "react-hook-form";
+
+
 import PageTemplate from "@/app/_components/globals/PageTemplate";
 import FloatingMenu from "@/app/_components/globals/FloatingMenu";
 import AgendaCard from "@/app/_components/agendas/AgendaCard";
 import Button from "@/app/_components/globals/Button";
 import Popup from "@/app/_components/globals/Popup";
-import { useAgendaService, useUserService } from "@/app/_services";
-import { useForm } from "react-hook-form";
 
-export default function Home() {
-  const [menuVisible, setMenuVisible] = useState(false);
-  const [newAgendaOverlay, setNewAgendaOverlay] = useState(true);
-  const agendaService = useAgendaService();
+// #endregion
+
+export default function Home() { 
+  // #region CHECK IF THE USER IS LOGGED IN
+
   const userService = useUserService();
-  const {register, handleSubmit, formState} = useForm();
   const user = userService.currentUser;
   useEffect(() => {
-    userService.getCurrent();
-}, [userService]);
+      userService.getCurrent();
+  }, [userService]);
+  
+  if (!user){
+    redirect('/')
+  }
+  //#endregion
 
+  // #region State and Hooks
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [newAgendaOverlay, setNewAgendaOverlay] = useState(true);
+
+  const {register, handleSubmit, formState} = useForm();
+  const agendaService = useAgendaService();
+  
+  // #endregion
 
   const fields ={
     name: register("name", {required: "Name Required!"}),
     ownerId: register("ownerId")
   }
-  if (!user){
-    throw ("Error: Not logged in")
-  }
+
 
   const agendas = ["Agenda 1", "Agenda 2", "Agenda 3", "Agenda 4"]
 
