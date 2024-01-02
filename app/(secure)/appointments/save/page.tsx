a6c5f224806a85513a327458db91f09b005df99c
Matheus Pesegoginski <pese.ek.tk@outlook.com>
2024-01-02 05:07:10 -0300

    Defined a Template for the page that saves new or edited appointments.

---
+++ app/(secure)/appointments/save/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { useState }       from "react";
+
+import PageTemplate       from "@/app/_components/PageTemplate";
+import Button             from "@/app/_components/Button";
+import FloatingMenu       from "@/app/_components/FloatingMenu";
+
+
+export default function Home(){
+
+      const ClientName = "Fulano da Silva";
+      const ServiceName = "Pedicure";
+      const ServiceDate = "01-01-2024";
+      const EmployeeName = "Ciclano de Souza"
+      const ServiceTime = "00:00"
+
+
+      const [menuVisible, setMenuVisible] = useState(false);
+  
+      const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+      };
+
+
+      const Header =(
+            <h2>Salvar Compromisso</h2>
+
+      );
+      const Body =(
+            <>
+            <div>
+                  <div>
+                        <div>Cliente</div>
+                        <div>{ClientName}</div>
+                  </div>
+                  <div>
+                        <div>Serviço</div>
+                        <div>{ServiceName}</div>
+                  </div>
+                  <div>
+                        <div>Data</div>
+                        <div>{ServiceDate}</div>
+                  </div>
+                  <div>
+                        <div>Funcionário</div>
+                        <div>{EmployeeName}</div>
+                  </div>
+                  <div>
+                        <div>Hora</div>
+                        <div>{ServiceTime}</div>
+                  </div>
+            </div>
+            {/* TODO: Create components for each div separation of a box, following the design in Figma */}
+
+            {menuVisible && <FloatingMenu />}
+            </>
+      );
+
+      const Footer=(
+            <div className="flex w-full justify-between gap-3">
+                  <Button onClick={toggleMenu}>Menu</Button>
+                  
+            </div>      
+      );
+
+
+      return(
+
+            <PageTemplate>
+                  {{Header, Body, Footer}}
+           </PageTemplate>
+      );
+
+
+}
