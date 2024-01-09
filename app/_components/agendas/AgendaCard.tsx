94b6cae1b2a07e69a6888ec7f9d341005c697855
Matheus Pesegoginski <pese.ek.tk@outlook.com>
2024-01-09 09:34:39 -0300

    ? testing feather-icons-react library

--- app/_components/agendas/AgendaCard.tsx
+++ app/_components/agendas/AgendaCard.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import { Trash2 } from 'react-feather';
 
 interface ButtonProps {
   children: ReactNode;
@@ -8,8 +9,11 @@ interface ButtonProps {
 
 const AgendaCard: React.FC<ButtonProps> = ({ children, onClick, className = "" }) => {
     return (
-      <button className={`${className} flex-1 w-full justify-center bg-[rgba(0,0,0,0.3)] text-left p-10`} onClick={onClick}>
-        {children}
+      <button className={`${className} flex flex-col flex-1 w-full justify-center bg-[rgba(0,0,0,0.3)] text-left p-10`} onClick={onClick}>
+        <div>{children}</div>
+        <Trash2 color='white' size={24}></Trash2>
+
+
       </button>
     );
 };
